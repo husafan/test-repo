1f2c6cb6eeae315a5fcd5265ea8d1a79667de5d8
Joshua Harrison <joshharrison@google.com>
2017-08-25 18:10:47 -0700

    Add Fake XCode project.

---
+++ Nothing/Nothing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nothing
+//
+//  Created by Joshua Harrison on 8/25/17.
+//  Copyright © 2017 StreetLevel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
